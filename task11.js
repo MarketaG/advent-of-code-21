cbfbdc771fbccffce68125861ab9537ff276a618
MarketaG <marketagrac@gmail.com>
2024-01-21 21:21:33 +0100

    Function to update the energy level, function to perform a simulation step, cycle 100 steps and output the result.

--- task11.js
+++ task11.js
@@ -1,3 +1,7 @@
+/*------------------------------------------------------------*/
+/*-Task 11: How many total flashes are there after 100 steps?-*/
+/*------------------------------------------------------------*/
+
 const input = [
   "5483143223",
   "2745854711",
@@ -14,3 +18,54 @@ const input = [
 // conversion of "input" into a two-dimensional matrix of numbers
 let matrix = input.map(row => row.split('').map(Number))
 let flashes = 0
+
+const energyLevelUpdate = (i, j, processedValues) => {
+  // condition checking whether it is a valid matrix coordinate
+  if (i < 0 || i >= matrix.length || j < 0 || j >= matrix[0].length) {
+    return
+  }
+
+  const key = `${i}:${j}`
+
+  //  condition checking the processing of the value
+  if (processedValues.has(key)) {
+    return
+  }
+
+  // incrementing the value of the matrix
+  matrix[i][j]++
+
+  // checking the matrix value if it exceeds the value of 9
+  if (matrix[i][j] > 9) {
+
+    // setting the value to 0 when it exceeds the value of 9
+    matrix[i][j] = 0
+    processedValues.add(key)
+
+    // incrementation of the flash count
+    flashes++
+
+    // recursive call for surrounding values
+    for (let x = -1; x <= 1; x++) {
+      for (let y = -1; y <= 1; y++) {
+        if (x === 0 && y === 0) continue
+        energyLevelUpdate(i + x, j + y, processedValues)
+      }
+    }
+  }
+}
+
+const step = () => {
+  const processedValues = new Set()
+
+  // iterates through all cells in the matrix and updates their energy level
+  matrix.forEach((row, i) =>
+    row.forEach((_, j) => energyLevelUpdate(i, j, processedValues))
+  )
+}
+
+for (let i = 0; i < 100; i++) {
+  step()
+}
+
+console.log("Number of flashes after 100 steps:", flashes)
