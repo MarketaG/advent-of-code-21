e03148f147997168d35c141a65c4b1209102dfee
MarketaG <marketagrac@gmail.com>
2024-01-22 02:44:45 +0100

    Creating the syntaxScoring function for analyzing code syntax with respect to bracket pairing and generating a score for syntax errors.

--- task10.js
+++ task10.js
@@ -17,6 +17,62 @@ const input = [
 
 const syntaxScoring = () => {
 
+  const stack = []
+  const closingBlock = {
+    "(": ")",
+    "{": "}",
+    "[": "]",
+    "<": ">"
+  }
+  const errorPoints = {
+    ")": 3,
+    "]": 57,
+    "}": 1197,
+    ">": 25137
+  }
+  const errorCount = {
+    ")": 0,
+    "]": 0,
+    "}": 0,
+    ">": 0
+  }
+
+  const openingBracket = (char) => {
+    // the test() method performs a match between a regular expression and a character
+    return /[({\[<]/.test(char)
+  }
+
+  const closingBracket = (char) => {
+    const expected = stack.pop()
+    // compares the expected and actual brackets
+    if (expected !== char) {
+      // in case of a mismatch, it increments the error count for the respective closing bracket
+      errorCount[char]++
+    }
+  }
+
+  const calculateResult = () => {
+    // the calculateResult function calculates the total syntax error score
+
+    return Object.keys(errorPoints)
+      .map(key => errorPoints[key] * errorCount[key])
+      .reduce((total, score) => total + score, 0)
+  }
+
+  for (const row of input) {
+    for (const char of row) {
+      if (openingBracket(char)) {
+        // the corresponding closing bracket to the currently found opening bracket is pushed onto the stack
+        stack.push(closingBlock[char])
+      } else {
+        // in the case of a different bracket that is not an opening bracket, the following function is called
+        closingBracket(char)
+      }
+    }
+  }
+
+  const totalResult = calculateResult()
+  console.log(`The total syntax error score for this file is: ${totalResult}`)
 }
 
 syntaxScoring()
